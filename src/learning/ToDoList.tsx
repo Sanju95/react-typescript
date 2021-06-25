00e6cb0159bfb5c1f6f4ebafbacff7183253f584
Sanju Krishnarajah <sanju.krishnarajah@woodmac.com>
2021-06-25 14:10:27 +0100

    Todo

--- src/learning/ToDoList.tsx
+++ src/learning/ToDoList.tsx
@@ -1,66 +1,33 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 
-type FormEl = React.FormEvent<HTMLFormElement>;
-interface Todo {
+interface TodoItems {
   name: string;
   status: boolean;
 }
 
 const ToDoList = () => {
-  const [value, setValue] = useState("");
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todo, setTodo] = useState<TodoItems[]>([]);
 
-  const handleSubmit = (e: FormEl) => {
-    e.preventDefault();
-    addTodo(value);
-    setValue("");
-  };
-
-  const addTodo = (name: string) => {
-    const newTodo: Todo[] = [...todos, { name, status: false }];
-    setTodos(newTodo);
-  };
-
-  const todoStatus = (index: number) => {
-    const newTodo: Todo[] = [...todos];
-    newTodo[index].status = !newTodo[index].status;
-    setTodos(newTodo);
-  };
-
-  const removeTodo = (index: number) => {
-    const newTodo: Todo[] = [...todos];
-    newTodo[index].status
-      ? newTodo.splice(index, 1)
-      : alert("To do not completed! You want to give up on this?");
-    setTodos(newTodo);
-  };
+  const handleAddTodo = () => {};
 
   return (
-    <Fragment>
-      <h1>To Do List</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        ></input>
-        <button type='submit'>Add</button>
-      </form>
-
+    <>
       <section>
-        {todos.map((item, index: number) => {
-          return (
-            <div key={index}>
-              {item.name}
-              <button onClick={() => todoStatus(index)}>
-                {item.status ? "Completed" : "Incomplete"}
-              </button>
-              <button onClick={() => removeTodo(index)}>&times;</button>
-            </div>
-          );
-        })}
+        <input type='text' placeholder='New Todo' />
+        <button
+          onClick={() => {
+            handleAddTodo;
+          }}
+        >
+          Add ToDo
+        </button>
       </section>
-    </Fragment>
+      <section>
+        <ul>
+          <li>{todo}</li>
+        </ul>
+      </section>
+    </>
   );
 };
 
